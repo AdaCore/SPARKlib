cf127f8b6f79ea3dd6033007f61168c6afd8292f
Claire Dross <dross@adacore.com>
2024-10-25 09:32:21 +0200

    Stop using Big_Integers_Ghost in the SPARKlib
    Make the SPARKlib were standalone to simplify the certification
    process. It decorrelates the SPARKlib from the use of SPARK in the
    certification of the light runtime.

---
+++ src/spark__exec.ads
@@ -0,0 +1,13 @@
+--
+--  Copyright (C) 2022-2024, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+--  This unit is meant to replace SPARK for internal testing only
+
+package SPARK with SPARK_Mode, Pure is
+
+   procedure Check_Or_Fail is null;
+
+end SPARK;
