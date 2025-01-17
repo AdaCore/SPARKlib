77e2e09e96d5166136779b6355bb3d50fc6dd6e7
Claire Dross <dross@adacore.com>
2025-01-17 11:59:02 +0100

    Add Ada_2022 pragma to hash table packages

--- src/spark-containers-formal-doubly_linked_lists.adb
+++ src/spark-containers-formal-doubly_linked_lists.adb
@@ -4,6 +4,8 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
+pragma Ada_2022;
+
 with SPARK.Containers.Stable_Sorting; use SPARK.Containers.Stable_Sorting;
 with System; use type System.Address;
 
