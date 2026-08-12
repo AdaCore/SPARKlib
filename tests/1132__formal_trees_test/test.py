27b2603d18f0538103405f4bc9bee55864360b34
Johannes Kanig <kanig@adacore.com>
2026-08-12 09:31:20 +0000

    Remove prover output from sparklib test
    no-issue-check

--- tests/1132__formal_trees_test/test.py
+++ tests/1132__formal_trees_test/test.py
@@ -2,5 +2,5 @@ from test_support import prove_all, sparklib_exec_test
 
 
 if __name__ == "__main__":
-    prove_all(sparklib=True, level=2, sparklib_bodymode=True)
+    prove_all(sparklib=True, level=2, sparklib_bodymode=True, report="all")
     sparklib_exec_test(sparklib_bodymode=True)
